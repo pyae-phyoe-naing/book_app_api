b08cb3dd4f538e4645d65544e83ba96bc86f7145
fazza7777 <aungbarlaay7777@gmail.com>
2022-11-18 13:50:41 +0630

    error handling and route error handling

--- app.js
+++ app.js
@@ -9,5 +9,16 @@ app.use(express.json()); // instead - BodyParser
 const catRoute = require('./route/category');
 
 app.use('/category', catRoute);
+app.use((err, req, res, next) => {
+    err.status = err.status || 500;
+    res.status(err.status).json({
+        con: false,
+        msg: err.message
+    });
+});
+
+app.use('*', (req, res, next) => res.status(200).json({
+    msg: "Route not found",
+}));
 
 app.listen(process.env.PORT, console.log(`Server is running at port ${process.env.PORT}`));
