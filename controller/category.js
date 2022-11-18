943cbcff7c9ef2486efcf9390b18c55915c83813
fazza7777 <aungbarlaay7777@gmail.com>
2022-11-18 14:34:53 +0630

    add category process

--- controller/category.js
+++ controller/category.js
@@ -1,12 +1,24 @@
 const DB = require('../model/category');
-const { responseMsg } = require('../utils/helper');
+const {
+    responseMsg
+} = require('../utils/helper');
 
 const all = async (req, res, next) => {
     let cats = await DB.find();
     responseMsg(res, true, 'All Category', cats);
 }
 const add = async (req, res, next) => {
-     responseMsg(res, true, 'All Category');
+    let existCat = await DB.findOne({
+        name: req.body.name
+    });
+    if (existCat) {
+        next(new Error('Category name is already in use!'));
+    } else {
+        console.log(req.body.name);
+        let cat = await new DB(req.body).save();
+        responseMsg(res, true, 'Success add new category', cat);
+    }
+
 }
 
 module.exports = {
